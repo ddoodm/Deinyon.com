9aa1447fca5d35039c8eb35e7ff14c5cc35500f7
Deinyon Davies <deinyond@gmail.com>
2020-10-25 16:26:11 +1100

    Cleanup and Azure publish settings

--- Deinyon2018React/ClientApp/components/About.tsx
+++ Deinyon2018React/ClientApp/components/About.tsx
@@ -7,18 +7,13 @@ export class About extends React.Component<{}, {}> {
             <hr />
             <p>
                 It's Deinyon's website! Pronounced like <i>Cayenne</i> &#x1f336;&#xFE0F;, but starting with "dein". Also known as <i>ddoodm</i> across the net.
-                    </p>
-            <p>
-                I'm trying to re-imagine my old website
-                        (<a href="https://ddoodm.com/" target="_blank">ddoodm.com</a>) as a React app...
-                        But it's taken over a year for me to be bothered to get this far! lol
-                    </p>
+            </p>
             <p>
                 Check out my <a href="https://ddoodm.com/" target="_blank">old website</a>,&nbsp;
                 <a href="https://github.com/ddoodm" target="_blank">GitHub</a>,&nbsp;
                 <a href="https://youtube.com/ddoodm" target="_blank">YouTube</a> and&nbsp;
                 <a href="https://blog.deinyon.com/" target="_blank">blog</a>
-                    </p>
+            </p>
         </div>;
     }
 }
